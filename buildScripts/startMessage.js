bf0852d7d89e12493e858e5b98577593b881d225
Steve Day <steve@Steves-MacBook-Pro.local>
2018-02-19 17:45:53 -0500

    Updated to ES6 import

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,4 +1,8 @@
 //chalk allows you to define output text colors to the consol
-var chalk = require('chalk');
+//var chalk = require('chalk');
+
+// Use ES6 syntax
+import chalk from 'chalk';
+
 
 console.log(chalk.green('Starting app in dev mode...'));
