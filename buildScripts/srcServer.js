b57b8a4f652842d4fae83a33ebcb076fff889e9b
Steve Day <steve@Steves-MacBook-Pro.local>
2018-02-19 15:30:39 -0500

    created webServer configuration file

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+var open = require('open');
+
+var port = 3000;
+var app = express();
+
+//Tell express which routes it should handle
+//Any references to the root will be handled by this function
+app.get('/', function(req, res){
+    //__dirname gets the current directory name
+    res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+//Tell express that we would like it to listen to the port we defined above
+app.listen(port, function(err){
+    if (err) {
+        console.log(err);
+    } else {
+        open('http://localhost:' + port);
+    }
+});
