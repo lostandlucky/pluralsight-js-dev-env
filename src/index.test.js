555d35dd0bb251b472bedc9125bda60135c869ce
Steve Day <steve@Steves-MacBook-Pro.local>
2018-03-04 18:54:35 -0500

    Added Automated Testing

---
+++ src/index.test.js
@@ -0,0 +1,23 @@
+import {expect} from 'chai';  //import the assertion library
+import jsdom from 'jsdom';
+import fs from 'fs';  //file system so you can read files
+
+//using arrow function for brevity, but could use regular functions
+describe('Our first test', () => {
+	it('should pass', () => {
+		expect(true).to.equal(true);
+	});
+});
+
+describe('index.html', () => {
+	it('should say hello', (done) => {  //when you do an asnch test, you have to pass done and call it later, so that Mocha knows the test is done
+		const index = fs.readFileSync('./src/index.html', "utf-8");  //get index.html into memory
+		//vv can optionally place JavaScript files to load into the JSDOM environemnt as the second parameter, but need to use isomorphic fetch
+		jsdom.env(index, function(err, window) {
+			const h1 = window.document.getElementsByTagName('h1')[0];  //give me a reference to the first h1 on the page
+			expect(h1.innerHTML).to.equal("Hello World!!!");
+			done();  //asynch test, so have to tell Mocha when done.
+			window.close();
+		});
+	});
+});
