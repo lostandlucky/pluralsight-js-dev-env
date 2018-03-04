85ec71227a8a080a7564e4ec367737fc2bfbe104
Steve Day <steve@Steves-MacBook-Pro.local>
2018-03-04 17:44:33 -0500

    Added Linting with filewatching

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -11,12 +11,13 @@ import path from 'path';
 import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev'
+/* eslint-disable no-console */
 
 const port = 3000;
 const app = express();
 const compiler = webpack(config);
 
-app.use(require('webpack-dev-middleware')(comiler,{
+app.use(require('webpack-dev-middleware')(compiler,{
 	noInfo: true,
 	publicPath: config.output.publicPath
 }));
