9fb359f3d1c20b4721c52f3d5b3ac83f107c2680
Steve Day <steve@Steves-MacBook-Pro.local>
2018-03-16 09:15:25 -0400

    Initial API buildout

--- src/index.test.js
+++ src/index.test.js
@@ -10,12 +10,12 @@ describe('Our first test', () => {
 });
 
 describe('index.html', () => {
-	it('should say hello', (done) => {  //when you do an asnch test, you have to pass done and call it later, so that Mocha knows the test is done
+	it('should say h1 that says Users', (done) => {  //when you do an asnch test, you have to pass done and call it later, so that Mocha knows the test is done
 		const index = fs.readFileSync('./src/index.html', "utf-8");  //get index.html into memory
 		//vv can optionally place JavaScript files to load into the JSDOM environemnt as the second parameter, but need to use isomorphic fetch
 		jsdom.env(index, function(err, window) {
 			const h1 = window.document.getElementsByTagName('h1')[0];  //give me a reference to the first h1 on the page
-			expect(h1.innerHTML).to.equal("Hello World!!!");
+			expect(h1.innerHTML).to.equal("Users");
 			done();  //asynch test, so have to tell Mocha when done.
 			window.close();
 		});
